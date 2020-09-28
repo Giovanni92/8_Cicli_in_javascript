bb32c85722f8b44b4abb8cdc8c9913ac3eb69a13
Giovanni <desiderio.giovanni92@gmail.com>
2020-09-28 18:29:07 +0200

    Revert "lezione su cicli for"
    This reverts commit a2ea7b3151073caca25a3112d562929ad1af08ce.

--- 11_ciclo_FOR.js
+++ 11_ciclo_FOR.js
@@ -3,20 +3,23 @@
 //Condizione: Viene verificata prima di ogni iterazione
 //Modifica: Viene eseguita dopo ogni ciclo
 
-// for(var i =0; i<10; i++){
-//     console.log(i);
-//   }
+for(var i =0; i<10; i++){
+    //console.log(i);
+  }
+  
+  
   
   for(var j =0;;j++){
     
-    //display solo num paro
-    if(j%2!=0){
-      continue;
-    }
-     if(j>9){
-       break;
-     }
-   console.log(j);
+      if(j%2!=0){
+        continue;
+      }
+       if(j>9){
+         break;
+       }
+     console.log(j);
+   
     
-}
-  
+    
+    
+  }
